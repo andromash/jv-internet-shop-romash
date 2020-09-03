3f1bedc17952850fc59ef9887d9306398b26c4fe
ROMASHSX <andriiromash@gmail.com>
2020-09-03 15:31:21 +0300

    DAO for all models are created

---
+++ src/main/java/com/internet/shop/dao/ShoppingCartDao.java
@@ -0,0 +1,19 @@
+package com.internet.shop.dao;
+
+import com.internet.shop.model.ShoppingCart;
+import java.util.List;
+import java.util.Optional;
+
+public interface ShoppingCartDao {
+    ShoppingCart create(ShoppingCart shoppingCart);
+
+    Optional<ShoppingCart> get(Long id);
+
+    Optional<ShoppingCart> getByUser(Long userId);
+
+    List<ShoppingCart> getAll();
+
+    ShoppingCart update(ShoppingCart shoppingCart);
+
+    boolean delete(Long id);
+}
---
+++ src/main/java/com/internet/shop/dao/impl/ShoppingCartDaoImpl.java
@@ -0,0 +1,48 @@
+package com.internet.shop.dao.impl;
+
+import com.internet.shop.dao.ShoppingCartDao;
+import com.internet.shop.db.Storage;
+import com.internet.shop.model.ShoppingCart;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+public class ShoppingCartDaoImpl implements ShoppingCartDao {
+    @Override
+    public ShoppingCart create(ShoppingCart shoppingCart) {
+        Storage.addShoppingCart(shoppingCart);
+        return shoppingCart;
+    }
+
+    @Override
+    public Optional<ShoppingCart> get(Long id) {
+        return Storage.shoppingCarts.stream()
+                .filter(cart -> cart.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public Optional<ShoppingCart> getByUser(Long userId) {
+        return Storage.shoppingCarts.stream()
+                .filter(cart -> cart.getUserId().equals(userId))
+                .findFirst();
+    }
+
+    @Override
+    public List<ShoppingCart> getAll() {
+        return Storage.shoppingCarts;
+    }
+
+    @Override
+    public ShoppingCart update(ShoppingCart shoppingCart) {
+        IntStream.range(0, Storage.shoppingCarts.size())
+                .filter(i -> Storage.shoppingCarts.get(i).getId().equals(shoppingCart.getId()))
+                .forEach(i -> Storage.shoppingCarts.set(i, shoppingCart));
+        return shoppingCart;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return Storage.shoppingCarts.removeIf(cart -> cart.getId().equals(id));
+    }
+}
