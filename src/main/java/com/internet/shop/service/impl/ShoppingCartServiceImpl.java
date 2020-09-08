1a33cc26eed66e85ac7edbf125325d230ad2fc73
ROMASHSX <andriiromash@gmail.com>
2020-09-08 10:54:13 +0300

    GenericDao and GenericService are created and implemented

--- src/main/java/com/internet/shop/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/com/internet/shop/service/impl/ShoppingCartServiceImpl.java
@@ -7,6 +7,7 @@ import com.internet.shop.model.Product;
 import com.internet.shop.model.ShoppingCart;
 import com.internet.shop.service.ShoppingCartService;
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ShoppingCartServiceImpl implements ShoppingCartService {
@@ -16,6 +17,31 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     @Override
     public ShoppingCart create(ShoppingCart shoppingCart) {
         return shoppingCartDao.create(shoppingCart);
+    }
+
+    @Override
+    public ShoppingCart get(Long id) {
+        return shoppingCartDao.get(id).get();
+    }
+
+    @Override
+    public List<ShoppingCart> getAll() {
+        return shoppingCartDao.getAll();
+    }
+
+    @Override
+    public ShoppingCart update(ShoppingCart item) {
+        return shoppingCartDao.update(item);
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return shoppingCartDao.delete(id);
+    }
+
+    @Override
+    public boolean delete(ShoppingCart shoppingCart) {
+        return shoppingCartDao.delete(shoppingCart.getId());
     }
 
     @Override
@@ -40,10 +66,5 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     @Override
     public ShoppingCart getByUserId(Long userId) {
         return shoppingCartDao.getByUser(userId).get();
-    }
-
-    @Override
-    public boolean delete(ShoppingCart shoppingCart) {
-        return shoppingCartDao.delete(shoppingCart.getId());
     }
 }
